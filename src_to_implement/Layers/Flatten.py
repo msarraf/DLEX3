7c0ebea76b16508d59e848878e7bc481ba932040
Mohammadhossein Sheikhsarraf <m.sheikhsarraf@gmail.com>
2022-01-01 15:24:58 +0100

    change Flatten.py to receive flatten input

--- src_to_implement/Layers/Flatten.py
+++ src_to_implement/Layers/Flatten.py
@@ -1,29 +1,25 @@
 import numpy as np
+from src_to_implement.Layers import Base
 
-
-class Flatten:
+class Flatten(Base.BaseLayer):
 
     def __init__(self):
-        self.trainable = False
-        self.batch_size = 0
-        self.feature_map = 0
-        self.feature_map_dim = 0
-        self.flatten_output = 0
+        super().__init__()
+        self.input_tensor = 0
+        self.error_tensor = 0
 
     def forward(self, input_tensor):
-       self.input_tensor = input_tensor
-       self.batch_size = self.input_tensor.shape[0]
-       self.feature_map = self.input_tensor.shape[1]
-       self.feature_map_dim = [self.input_tensor.shape[2], self.input_tensor.shape[3]]
-       tensor_elements = self.feature_map * self.feature_map_dim[0] * self.feature_map_dim[1]
-       self.flatten_output = np.reshape(np.ravel(self.input_tensor), (self.input_tensor.shape[0], tensor_elements))
-       return self.flatten_output
+        self.input_tensor = input_tensor
+        if len(self.input_tensor.shape) > 2:
+            return self.input_tensor.reshape(self.input_tensor.shape[0], self.input_tensor.shape[1]*self.input_tensor.shape[2]*self.input_tensor.shape[3])
+        else:
+            return input_tensor
 
-    def backward(self,error_tensor):
-        self.error_tensor = error_tensor
-        reshaped_flatten = np.reshape(self.error_tensor, (self.batch_size, self.feature_map, self.feature_map_dim[0],self.feature_map_dim[1]))
-        return reshaped_flatten
-
-
-
-
+    def backward(self, error_tensor):
+        if len(self.input_tensor.shape) > 2:
+            self.error_tensor = error_tensor
+            reshaped_flatten = self.error_tensor.reshape(self.input_tensor.shape)
+            return reshaped_flatten
+        else:
+            self.error_tensor = error_tensor
+            return self.error_tensor
