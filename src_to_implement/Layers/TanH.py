4bc76cf7c3d90c13379bacc152b1d67eec5be397
Mohammadhossein Sheikhsarraf <m.sheikhsarraf@gmail.com>
2022-01-08 12:26:00 +0100

    Complete TanH.py

--- src_to_implement/Layers/TanH.py
+++ src_to_implement/Layers/TanH.py
@@ -1,14 +1,15 @@
-
+from src_to_implement.Layers import Base
 import numpy as np
-class TanH:
-    def __init__(self):
-        pass
 
-    def forward(self,input_tensor):
-        self.input_tensor = input_tensor
-        self.out_TanH = np.tanh(self.input_tensor)
+
+class TanH(Base.BaseLayer):
+    def __init__(self):
+        self.out_TanH = None
+        super().__init__()
+
+    def forward(self, input_tensor):
+        self.out_TanH = np.tanh(input_tensor)
         return self.out_TanH
 
-    def backward(self,error_tensor):
-        self.error_tensor = error_tensor
-        return (1 - self.out_TanH**2) * self.error_tensor
+    def backward(self, error_tensor):
+        return (1 - self.out_TanH ** 2) * error_tensor
