2b224dc0b7bf703cc5943672ca7aded4563d2dd3
Mohammadhossein Sheikhsarraf <m.sheikhsarraf@gmail.com>
2022-01-08 12:25:34 +0100

    Complete Sigmoid.py

--- src_to_implement/Layers/Sigmoid.py
+++ src_to_implement/Layers/Sigmoid.py
@@ -1,14 +1,15 @@
 import numpy as np
+from src_to_implement.Layers import Base
 
-class Sigmoid:
+
+class Sigmoid(Base.BaseLayer):
     def __init__(self):
-        pass
+        self.out_sigmoid = None
+        super().__init__()
 
-    def forward(self,input_tensor):
-        self.input_tensor = input_tensor
-        self.out_sigmoid = 1/(1 + np.exp(-self.input_tensor))
+    def forward(self, input_tensor):
+        self.out_sigmoid = 1 / (1 + np.exp(-1*input_tensor))
         return self.out_sigmoid
 
-    def backward(self,error_tensor):
-        self.error_tensor = error_tensor
-        return self.out_sigmoid * (1 - self.out_sigmoid) * self.error_tensor
+    def backward(self, error_tensor):
+        return self.out_sigmoid * (1 - self.out_sigmoid) * error_tensor
