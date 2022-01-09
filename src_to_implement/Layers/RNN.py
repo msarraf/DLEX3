6865857f568e06e04afd68545e55d9e1b9a95e3d
Mohammadhossein Sheikhsarraf <m.sheikhsarraf@gmail.com>
2022-01-09 17:07:33 +0100

    Update RNN.py

--- src_to_implement/Layers/RNN.py
+++ src_to_implement/Layers/RNN.py
@@ -1,3 +1,5 @@
+import copy
+
 from src_to_implement.Layers import Base
 import numpy as np
 from src_to_implement.Layers import TanH, Sigmoid, FullyConnected
@@ -14,6 +16,10 @@ class RNN(Base.BaseLayer):
         self.belong_to_long_sequence = False
         self.first_state = np.zeros((1, hidden_size))
         self.hidden_state = None
+        self.fully_connected_states_tanh = None
+        self.gradient_weights_fc_state = None
+        self.fullyConnected_input_tensor = None
+        self.optimizer = None
         self.tanH = TanH.TanH()
         self.Sigmoid = Sigmoid.Sigmoid()
         self.fullyConnected_output = FullyConnected.FullyConnected(hidden_size, output_size)
@@ -22,12 +28,18 @@ class RNN(Base.BaseLayer):
     def forward(self, input_tensor):
         self.batch_size = input_tensor.shape[0]
         self.hidden_state = np.zeros((self.batch_size, self.hidden_size))
+        self.fully_connected_states_tanh = np.zeros((self.batch_size, self.hidden_size))
+        self.fullyConnected_input_tensor = np.zeros((self.batch_size, self.hidden_size+self.input_size+1))
         if self.belong_to_long_sequence:
             self.hidden_state[0, :] = self.first_state
+        else:
+            self.first_state = np.zeros((1, self.hidden_size))
         for batch in range(self.batch_size):
-            state_input = np.append(self.first_state, input_tensor[batch, :].reshape(1, self.input_size), 1)
-            fully_connected_states = self.fullyConnected_states.forward(state_input)
-            fully_connected_states = self.tanH.forward(fully_connected_states)
+            state_input = np.append(self.first_state.reshape(1, self.hidden_size), input_tensor[batch, :].reshape(1, self.input_size), 1)
+            fully_connected_state_output = self.fullyConnected_states.forward(state_input)
+            self.fullyConnected_input_tensor[batch, :] = self.fullyConnected_states.get_input_tensor()
+            fully_connected_states = self.tanH.forward(fully_connected_state_output)
+            self.fully_connected_states_tanh[batch, :] = fully_connected_states
             self.hidden_state[batch, :] = fully_connected_states
             self.first_state = fully_connected_states
         output = self.fullyConnected_output.forward(self.hidden_state)
@@ -35,18 +47,28 @@ class RNN(Base.BaseLayer):
         return output
 
     def backward(self, error_tensor):
+        self.gradient_weights_fc_state = 0
         bp_output = np.zeros((self.batch_size, self.input_size))
         bp_out_tensor = self.Sigmoid.backward(error_tensor)
         bp_out_tensor = self.fullyConnected_output.backward(bp_out_tensor)
         bp_tensor_tanh_befor = np.zeros((1, self.hidden_size))
         for batch in reversed(range(self.batch_size)):
-            self.tanH.out_TanH = self.hidden_state[batch, :]
+            # because we call forward several times we must again initialize tanH out put for each forward
+            self.tanH.set_out_tanh(self.fully_connected_states_tanh[batch, :])
             # every state output relate to the previous inputs
-            bp_tensor_tanh = bp_out_tensor[batch, :] + bp_tensor_tanh_befor
+            bp_tensor_tanh = bp_out_tensor[batch, :].reshape(1, self.hidden_size) + bp_tensor_tanh_befor.reshape(1, self.hidden_size)
             bp_states = self.tanH.backward(bp_tensor_tanh)
-            bp_states = self.fullyConnected_states.backward(bp_states)
-            bp_tensor_tanh_befor = bp_states
-            bp_output[batch, :] = bp_states
+            # because we call forward several times we must again initialize input tensor for each backward
+            self.fullyConnected_states.set_input_tensor(self.fullyConnected_input_tensor[batch, :].reshape(1, self.input_size+self.hidden_size+1))
+            bp_states_and_input = self.fullyConnected_states.backward(bp_states)
+            bp_tensor_tanh_befor = bp_states_and_input[0, 0:self.hidden_size]
+            bp_output[batch, :] = bp_states_and_input[0, self.hidden_size:]
+            self.gradient_weights_fc_state += self.fullyConnected_states.get_gradient_weights()
+        if self.optimizer is not None:
+            weights_fullyconnected_states = self.fullyConnected_states.get_weights()
+            weights_fullyconnected_states = self.optimizer.calculate_update(weights_fullyconnected_states, self.gradient_weights_fc_state)
+            self.fullyConnected_states.set_weights(weights_fullyconnected_states)
+        return bp_output
 
     def set_belong_to_long_sequence(self, value):
         self.belong_to_long_sequence = value
@@ -55,3 +77,32 @@ class RNN(Base.BaseLayer):
         return self.belong_to_long_sequence
 
     memorize = property(get_belong_to_long_sequence, set_belong_to_long_sequence)
+
+    def initialize(self, weights_initializer, bias_initializer):
+        self.fullyConnected_states.initialize(weights_initializer, bias_initializer)
+        self.fullyConnected_output.initialize(weights_initializer, bias_initializer)
+
+    def get_weights(self):
+        return self.fullyConnected_states.get_weights()
+
+    def set_weights(self, values):
+        self.fullyConnected_states.set_weights(values)
+
+    weights = property(get_weights, set_weights)
+
+    def get_gradient_weights(self):
+        return self.gradient_weights_fc_state
+
+    def set_gradient_weights(self, value):
+        self.gradient_weights_fc_state = value
+
+    gradient_weights = property(get_gradient_weights, set_gradient_weights)
+
+    def get_optimizer(self):
+        return self.optimizer
+
+    def set_optimizer(self, value):
+        self.optimizer = copy.deepcopy(value)
+        self.fullyConnected_output.optimizer = copy.deepcopy(value)
+
+    Optimizers = property(get_optimizer, set_optimizer)
